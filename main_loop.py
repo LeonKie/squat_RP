7e996fa00aad8a2464a265607a6d28e3b2514b33
Leon Kiesgen <leon.kiesgen@web.de>
2020-09-21 19:57:21 +0200

    further setup

--- main_loop.py
+++ main_loop.py
@@ -41,7 +41,7 @@ def facedetector():
             #calc average y position
             avgHeight=  y+height/2
             print("AvgHeight: ", avgHeight)
-            if avgHeight>50:
+            if avgHeight>120:
                 return 2;
             else:
                 return 1;
